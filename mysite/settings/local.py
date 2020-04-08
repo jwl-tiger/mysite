fe7a6cd4ea926619397e5fb934df628bc48d0141
jwl-tiger <1185127147@qq.com>
2020-04-08 20:04:32 +0800

    第四次提交

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -16,7 +16,8 @@ from .base import *
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hkpgq^t%n#$a7e9pthhq1&a2xezjbx+a2l0r+-3fv2%m@y7*nd'
+#SECRET_KEY = 'hkpgq^t%n#$a7e9pthhq1&a2xezjbx+a2l0r+-3fv2%m@y7*nd'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
